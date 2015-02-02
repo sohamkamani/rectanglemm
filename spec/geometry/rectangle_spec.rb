ce23f060187229c8bcee86d6d44ec843ff91142d
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-02 13:18:09 +0530

    made test for mm also

--- spec/geometry/rectangle_spec.rb
+++ spec/geometry/rectangle_spec.rb
@@ -3,9 +3,16 @@ require 'spec_helper'
 describe 'Rectangle' do
 
 	let(:rectangle1) {Rectangle.new(Dimension.meter(1),Dimension.cm(50))}
+	let(:rectangle2) {Rectangle.new(Dimension.meter(1),Dimension.mm(50))}
+
 
 	it 'measures perimeter' do
 		expect(rectangle1.perimeter).to eq(3000)
 	end
+
+	it 'measures perimeter' do
+		expect(rectangle2.perimeter).to eq(2100)
+	end
+	
 	
 end
